3005a05d7c3c96d2d5871a5a44c728ce9e1bd308
Mavi5002 <125463443+Mavi5002@users.noreply.github.com>
2024-03-04 11:57:32 -0300

    Add files via upload

---
+++ atividade2/futebolarte/Comuns/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from Comuns.models import Pais,Estado,Cidade
+
+@admin.register(Pais)
+class PaisAdmin(admin.ModelAdmin):
+  list_display = ['nome']
+
+class CidadeInline(admin.TabularInline):
+  model = Cidade
+  extra = 0
+
+@admin.register(Estado)
+class EstadoAdmin(admin.ModelAdmin):
+  list_display = ['nome', 'sigla']
+  inlines = [CidadeInline]
+
+
+# Register your models here.
