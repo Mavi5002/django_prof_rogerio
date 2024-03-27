4185957001dca6c352a5780a33ed066a246cf9d9
Mavi5002 <125463443+Mavi5002@users.noreply.github.com>
2024-03-27 16:19:41 -0300

    Update views.py

--- erickgym/alunos/cadastro/views.py
+++ erickgym/alunos/cadastro/views.py
@@ -1,24 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from cadastro.models import Aluno
-from cadastro.serializers import AlunoSerializer
+from alunos.models import Aluno
+from alunos.serializers import AlunoSerializer
 from django.http import Http404
 from rest_framework import status
 
 
 class AlunoView(APIView):
-    def get_aluno(self,pk,usuario):
-        try:
-            return Aluno.objects.get(pk=pk,usuario=usuario)
-        except Aluno.DoesNotExist:
-            raise Http404
-
-
     def get(self,resquest):
         alunos = Aluno.objects.all()
         serializer = AlunoSerializer(alunos, many = True)
         return Response(serializer.data,status = 200)
-    
+     
     def post(self,request):
         serializer = AlunoSerializer(data=request.data)
         if serializer.is_valid():
@@ -26,19 +19,31 @@ class AlunoView(APIView):
             return Response(serializer.data, status=201)
         else:
             return Response(serializer.errors,status=400)
+
+
+class DetalheAluno(APIView):
+    def get_aluno(self,pk):
+        try:
+            return Aluno.objects.get()
+        except Aluno.DoesNotExist:
+            raise Http404
+
+    
+    def get(self,request,pk):
+        aluno = self.get_aluno(pk)
+        serializer = AlunoSerializer(aluno)
+        return Response(serializer.data)
     
     def put(self,request,pk):
-        aluno = self.get_aluno(pk,request.user)
+        aluno = self.get_aluno(pk)
         serializer = AlunoSerializer(aluno, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        else:
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self,request,pk):
-        aluno = self.get_aluno(pk,request.user)
+        aluno = self.get_aluno(pk)
         aluno.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-# Create your views here.
