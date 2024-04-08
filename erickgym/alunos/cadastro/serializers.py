abe4169199e02f6eebcb6053e857fbffa7f75984
Mavi5002 <125463443+Mavi5002@users.noreply.github.com>
2024-04-08 12:41:07 -0300

    Delete erickgym/alunos/cadastro directory

--- erickgym/alunos/cadastro/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Aluno
-
-class AlunoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Aluno
-        fields = ['nome','sexo','dt_nasc','tel','cpf']
